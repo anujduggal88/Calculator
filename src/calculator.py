7ace1d22cb97c837313c3b96d4c5bf01b584488c
Anuj Duggal <aduggal@aduggal.(none)>
2015-06-08 10:03:55 +0530

    Added Sub functionality.
    Signed-off-by: Anuj Duggal <aduggal@aduggal.(none)>

--- src/calculator.py
+++ src/calculator.py
@@ -10,6 +10,8 @@
 #
 
 # FUNCTION DEFINITIONS:
+def sub(n1, n2):
+    return (n1-n2)
 
 # INITIAL MESSAGE:
 print "Welcome to Calculator Function"
@@ -23,3 +25,8 @@ choice = input("Enter choice (1/2/3/4): ")
 
 num1 = int(input("Enter 1st number: "))
 num2 = int(input("Enter 2nd number: "))
+
+if choice is 2:
+    c = sub(num1, num2)
+    print c
+
